1b6dbb925db693a95f77c01b6f27129e2d0a5bd0
Matt McManis <mattmcmanis@outlook.com>
2020-12-20 01:36:43 -0800

    Update AssemblyInfo.cs

--- src/Ultra UI/Ultra UI/Properties/AssemblyInfo.cs
+++ src/Ultra UI/Ultra UI/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Ultra UI")]
-[assembly: AssemblyCopyright("Copyright © MattMcManis 2019")]
+[assembly: AssemblyCopyright("Copyright © MattMcManis 2019-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.0.0")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyVersion("0.3.1.0")]
+[assembly: AssemblyFileVersion("0.3.1.0")]
